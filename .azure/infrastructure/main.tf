4b0e88606f4ee968084b0158f631e88a49fe1020
Tobias Holzner <tobiasholzner@mailbox.org>
2022-06-11 15:14:40 +0200

    Add vulnerable terraform template

--- .azure/infrastructure/main.tf
+++ .azure/infrastructure/main.tf
@@ -1,1 +1,26 @@
-TODO:
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg-tfscan-demo" {
+  name     = "rg-tfscan-demo"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "satfscandemo" {
+  name                      = "satfscandemo"
+  resource_group_name       = azurerm_resource_group.rg-tfscan-demo.name
+  location                  = azurerm_resource_group.rg-tfscan-demo.location
+  account_tier              = "Standard"
+  account_replication_type  = "GRS"
+  enable_https_traffic_only = false
+}
