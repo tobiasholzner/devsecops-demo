9d604b5c7c1f6d54f59fd0a375364f934c9806db
Tobias Holzner <tobiasholzner@mailbox.org>
2022-06-11 13:28:40 +0200

    initial commit

---
+++ src/WebApp/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System.Diagnostics;
+using System.Net.Http;
+using System.Reflection;
+using System.Runtime.Versioning;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using sample_mvc.Models;
+
+namespace sample_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly Settings _settings;
+        private readonly IHttpClientFactory _clientFactory;
+
+        public HomeController(IOptions<Settings> settings, IHttpClientFactory clientFactory)
+        {
+            _settings = settings.Value;
+            _clientFactory = clientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var framework = Assembly
+                .GetEntryAssembly()?
+                .GetCustomAttribute<TargetFrameworkAttribute>()?
+                .FrameworkName;
+
+            ViewBag.Platform = new
+            {
+                OsVersion = System.Runtime.InteropServices.RuntimeInformation.OSDescription,
+                DotnetCoreVersion = framework
+            };
+
+            try
+            {
+                var httpResponse = await _clientFactory.CreateClient()
+               .GetAsync($"{_settings.TextUrl}");
+
+                if (httpResponse.IsSuccessStatusCode)
+                {
+                    var content = await httpResponse.Content.ReadAsStringAsync();
+
+                    ViewBag.Title = content;
+                    ViewBag.Image = _settings.Image;
+                    ViewBag.FredName = _settings.FredName;
+                }
+            }
+            catch (System.Exception)
+            {
+                ViewBag.Title = _settings.Title;
+                ViewBag.Image = _settings.Image;
+                ViewBag.FredName = _settings.FredName;
+            }
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/WebApp/Settings.cs
@@ -0,0 +1,13 @@
+﻿namespace sample_mvc
+{
+    public sealed class Settings
+    {
+        public string Title { get; set; }
+
+        public string TextUrl { get; set; }
+
+        public string Image { get; set; }
+
+        public string FredName { get; set; }
+    }
+}
