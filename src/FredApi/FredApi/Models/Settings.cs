fb4872b49a92982937f49c0447f200495b206fcf
Tobias Holzner <tobiasholzner@mailbox.org>
2022-06-11 14:06:49 +0200

    initial commit

--- src/FredApi/FredApi/Models/Settings.cs
+++
@@ -1,7 +0,0 @@
-namespace FredApi.Models
-{
-    public class Settings
-    {
-        public string Value {get; set;}
-    }
-}
--- src/FredApi/FredApi/Models/TextModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace FredApi.Models
-{
-    public class TextModel
-    {
-        public String text { get; set; }
-    }
-}
